c864464f092ea6a0726a04285a0d2e4cb34269f7
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-28 15:00:26 +0200

    prova axios

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 // Attraverso l'apposita API di Boolean
 // https://flynn.boolean.careers/exercises/api/random/mail
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
-// Bonus
+
+// BONUS
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
 
@@ -10,10 +11,21 @@ const {createApp} = Vue;
 
 createApp({
 
-    data(){
-        return{
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((resp) => {
+                console.log(resp);
+            })
             
         }
-    }
+    },
+
+    data(){
+        return{
+            mailAddresses: [],
+        }
+    },
 
 }).mount("#app");
